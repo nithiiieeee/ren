c5df1ed093159f0bf65eec6dac0a37b4b6958eff
🏳️ <71541854+nithiiieeee@users.noreply.github.com>
2026-08-05 20:59:38 +0530

    Add files via upload

---
+++ playlists.js
@@ -0,0 +1,27 @@
+// --- RADIO PLAYLIST ---
+const trackPlaylist = [
+    { name: "LILO-FI", url: "https://azurecloud.pvtwebs.com/listen/lilo/radio.mp3" },
+    { name: "LOFI 24-7", url: "https://ec3.yesstreaming.net:3755/stream" },
+    { name: "MOE Lofi", url: "https://stream-178.surfernetwork.com/v5reddyk8rhvv" },
+    { name: "FLUX CHILLHOP", url: "https://streams.fluxfm.de/Chillhop/aac-64/streams.fluxfm.de/" },
+    { name: "CHILLOFI", url: "https://streams.dez.ovh/listen/chillofi/radio.mp3" },
+    { name: "LAUT LOFI", url: "https://lofi.stream.laut.fm/lofi" },
+    { name: "NIA LO-FI", url: "https://radio.nia.nc/radio/8020/lofi-hq-stream.aac" },
+    { name: "LO-FIRE", url: "https://lo-firefm.stream.laut.fm/lo-firefm" },
+    { name: "SYNTHWAVE", url: "https://stream.synthwaveradio.eu/listen/synthwaveradio.eu/radio.mp3" },
+    { name: "ANIME", url: "https://stream.animeradio.de/animeradio.mp3" },
+    { name: "VAPORWAVE", url: "https://ice6.somafm.com/vaporwaves-64-aac" },
+    { name: "K-POP", url: "https://play.radioking.io/k-pop-highway-radio" },
+];
+
+// --- AMBIENT SOUND SOURCES ---
+const ambientSources = {
+    rain: "https://lilofiradio.com/_nuxt/rain.CUctS49_.mp3",
+    vinyl: "https://lilofiradio.com/_nuxt/vinyl.r3TP7i0m.mp3",
+    fire: "https://lilofiradio.com/_nuxt/fire.D-3nj1MV.mp3",
+    ocean: "https://lilofiradio.com/_nuxt/ocean.ECLxH3fW.mp3",
+    tape: "https://lilofirizer.com/_nuxt/tape.CbakOG-B.mp3",
+    woods: "https://lilofiradio.com/_nuxt/woods.B-sh45Ha.mp3",
+    river: "https://lilofiradio.com/_nuxt/river.BqLXToDa.mp3",
+    storm: "https://lilofiradio.com/_nuxt/storm.DqMuEy03.mp3"
+};
